28e6a0297753c5f84519240a4cc4ecd3c4aebfaa
Jesús.T <jesus@jetiradoro.com>
2024-08-14 20:55:58 +0200

    test

--- src/components/example/example-router.js
+++ src/components/example/example-router.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const ExampleController = require("./controllers/ExampleController");
 
 router.get("/", ExampleController.test);
+router.get("/test", (req, res) => {
+  res.json({ message: "Test" });
+});
 
 module.exports = router;
