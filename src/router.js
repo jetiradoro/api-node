96611962a3cb249b26cfa38e9c0d38bd1203993a
Jesús.T <jesus@jetiradoro.com>
2024-08-16 15:02:09 +0200

    docs router commented

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const ExampleRouter = require("./components/example/example-router")
-const DocRouter = require("./components/api-docs/routes")
+// const DocRouter = require("./components/api-docs/routes")
 const authSchema = require("./middlewares/schemas_validation/authSchema")
 const authMiddleware = require("./middlewares/authMiddleware")
 const dbRouter = require("./components/dragon-ball-api/db-router")
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
   res.json("Bienvenido a la API de Smartfulcode!")
 })
 
-router.use('/docs', DocRouter)
+// router.use('/docs', DocRouter)
 
 router.use("/example", 
   authSchema,
